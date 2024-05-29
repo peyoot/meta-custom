08d9f3c62c6ef96ab4f4da3bf6a66fa78ffdae02
peyoot <peyoot@hotmail.com>
2024-05-28 21:42:02 -0500

    fix qt5 in cc93 issue, correctly check wayland display

---
+++ recipes-qt/qtbase/qt5.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export QT_QPA_PLATFORM="wayland"
+
+[ -f "/etc/profile.d/weston.sh" ] || [ -f "/etc/profile.d/weston-socket.sh" ] && return
+
+
+export QT_QPA_PLATFORM="xcb"
+
+# Use EGLFS platform plugin for images without XWayland and X11
+[ -f "/etc/xserver-nodm/Xserver" ] || export QT_QPA_PLATFORM="eglfs" QT_QPA_EGLFS_INTEGRATION="eglfs_viv"
---
+++ recipes-qt/qtbase/qtbase_%.bbappend
@@ -0,0 +1,15 @@
+# qtbase.bbappend
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+
+SRC_URI:append = " \
+    file://qt5.sh \
+"
+
+do_install:append() {
+
+    install -d ${D}${sysconfdir}/profile.d/
+    install -m 0755 ${WORKDIR}/qt5.sh ${D}${sysconfdir}/profile.d/qt5.sh
+}
+
+
+FILES:${PN} += "${sysconfdir}/profile.d/qt5.sh"
