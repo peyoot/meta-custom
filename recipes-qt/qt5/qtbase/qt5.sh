f334913a5d3e88d51c1014461815b47dceeea9e9
peyoot <peyoot@hotmail.com>
2024-06-09 12:11:42 +0000

    update qt5.sh

--- recipes-qt/qt5/qtbase/qt5.sh
+++ recipes-qt/qt5/qtbase/qt5.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-export QT_QPA_PLATFORM="wayland"
-
-[ -f "/etc/profile.d/weston.sh" ] || [ -f "/etc/profile.d/weston-socket.sh" ] && return
-
-
-export QT_QPA_PLATFORM="xcb"
-
-# Use EGLFS platform plugin for images without XWayland and X11
-[ -f "/etc/xserver-nodm/Xserver" ] || export QT_QPA_PLATFORM="eglfs" QT_QPA_EGLFS_INTEGRATION="eglfs_viv"
-
 # Check for ROS environment and source the setup script based on the shell type
 if [ -d /etc/profile.d/ros/ ]; then
     # Check for bash shell
@@ -24,3 +14,13 @@ if [ -d /etc/profile.d/ros/ ]; then
 fi
 
 export QMLSCENE_DEVICE="softwarecontext"
+
+export QT_QPA_PLATFORM="wayland"
+
+[ -f "/etc/profile.d/weston.sh" ] || [ -f "/etc/profile.d/weston-socket.sh" ] && return
+
+
+export QT_QPA_PLATFORM="xcb"
+
+# Use EGLFS platform plugin for images without XWayland and X11
+[ -f "/etc/xserver-nodm/Xserver" ] || export QT_QPA_PLATFORM="eglfs" QT_QPA_EGLFS_INTEGRATION="eglfs_viv"
